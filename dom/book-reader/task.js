6c05875f328285e21d015e5e3a648fb0cfab0e27
maximle <mletunovskiy@yandex.ru>
2019-07-24 21:58:33 +0300

    task.js modified

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,17 +1,21 @@
 let fontLink = document.querySelectorAll('a.font-size');
 let fontSize = document.querySelector('div.book');
-
+let textColorLink = document.querySelectorAll('a.color');
+let bg1 = document.querySelector('div.book__control_background');
+let bg = bg1.querySelectorAll('a.color');
 console.log(fontLink);
-console.log(fontSize);
+console.log(bg);
 
 let fontSizeKey;
 let fontSizeKey1;
-
-currentFont = 1;
-
+let fontColor;
+let currentFont = 1;
+let currentColor = 0;
+let currentBg = 2;
 for (let i = 0; i < fontLink.length; i++) {
     fontLink[i].addEventListener('click', changeFont);
-    
+    textColorLink[i].addEventListener('click', changeFontColor);
+    bg[i].addEventListener('click', changeBgColor);
     function changeFont(event) {
         event.preventDefault();
         fontLink[currentFont].classList.remove('font-size_active');
@@ -34,7 +38,57 @@ for (let i = 0; i < fontLink.length; i++) {
         } else if (fontSizeKey1 === 'big') {
             fontSize.classList.add('book_fs-big');
         };
-        currentFont = i;      
+        currentFont = i;
+        
+        
+    };
+
+    function changeFontColor(event) {
+        event.preventDefault();
+        textColorLink[currentColor].classList.remove('color_active');
+        textColorKey = textColorLink[currentColor].getAttribute('data-color');
+        if (textColorKey === 'gray') {
+            fontSize.classList.remove('book_color-gray');
+        } else if (textColorKey === 'whitesmoke') {
+            fontSize.classList.remove('book_color-whitesmoke');
+        };
+        
+        textColorLink[i].classList.add('color_active');        
+        textColorKey1 = textColorLink[i].getAttribute('data-color');
+        console.log(textColorKey1);
+
+        if (textColorKey1 === 'gray') {
+            fontSize.classList.add('book_color-gray');
+        } else if (textColorKey1 === 'whitesmoke') {
+            fontSize.classList.add('book_color-whitesmoke');
+        };
+        currentColor = i;
+
+    };
+
+    
+
+    function changeBgColor(event) {
+        event.preventDefault();
+        bg[currentBg].classList.remove('color_active');
+        bgColorKey = bg[currentBg].getAttribute('data-color');
+        if (bgColorKey === 'gray') {
+            fontSize.classList.remove('book_bg-gray');
+        } else if (bgColorKey === 'black') {
+            fontSize.classList.remove('book_bg-black');
+        };
+        
+        bg[i].classList.add('color_active');        
+        bgColorKey1 = bg[i].getAttribute('data-color');
+        console.log(bgColorKey1);
+
+        if (bgColorKey1 === 'gray') {
+            fontSize.classList.add('book_bg-gray');
+        } else if (bgColorKey1 === 'black') {
+            fontSize.classList.add('book_bg-black');
+        };
+        currentBg = i;
+
     };
 
 };
